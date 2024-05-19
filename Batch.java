b643855f70ec526ca0f97466a7b7879d895d7337
PrabhakarOP <106029135+PrabhakarOP@users.noreply.github.com>
2024-05-19 18:50:17 +0530

    Added entities required

---
+++ Batch.java
@@ -0,0 +1,54 @@
+package feedbackManagementSystem.entity;
+
+import java.util.ArrayList;
+
+public class Batch {
+    //attribute
+    private String name;
+    private String bId;
+    private ArrayList<Student> studentList= new ArrayList<>();
+    private ArrayList<Feedback> feedbackList=new ArrayList<>();
+
+    //Contructor
+    public Batch(String name, String bId) {
+        this.name = name;
+        this.bId = bId;
+    }
+
+    //getter & setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getbId() {
+        return bId;
+    }
+
+    public void setbId(String bId) {
+        this.bId = bId;
+    }
+
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public ArrayList<Feedback> getFeedbackList() {
+        return feedbackList;
+    }
+
+    public void setFeedbackList(ArrayList<Feedback> feedbackList) {
+        this.feedbackList = feedbackList;
+    }
+
+    public void addStudentToBatch(Student student){
+        studentList.add(student);
+    }
+}
